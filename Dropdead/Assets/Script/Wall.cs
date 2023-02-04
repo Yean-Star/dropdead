d4f7af3d6f7b4dc0aae237a7f2ac89bbc13b9333
Thinnapat <116701758+MellyWard@users.noreply.github.com>
2023-02-04 11:41:50 +0700

    minorchange2

--- Dropdead/Assets/Script/Wall.cs
+++ Dropdead/Assets/Script/Wall.cs
@@ -6,19 +6,65 @@ public class Wall : MonoBehaviour
 {
     Rigidbody2D _rb;
     private int[] _angle = { 0, 90, 180, 270 };
-    
+    int rando;
+    int rotate;
+    public bool rotateEnable = false;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        rando = Random.Range(1, 10);
+    }
     void Start()
     {
         int rand = Random.Range(0, _angle.Length);
         transform.rotation = Quaternion.Euler(0, 0, _angle[rand]);
 
-        //int inde = Random.Range(0, _angle.Length);
+        Debug.Log(rando);
+        if(rando == 2&&rotateEnable)
+        {
+            //Debug.Log("rotate");
+            StartCoroutine(RotateCouterClockWise());
+        }
+        if (rando == 1&&rotateEnable)
+        {
+            //Debug.Log("rotate");
+            StartCoroutine(RotateClockWise());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //rotate++;
+        //switch(rando){
+        //    case 0:
+        //        transform.rotation = Quaternion.Euler(0, 0, rotate);
+        //        break;
+        //    case 1:
+
+        //        break;
+        //}
+    }
+
+    private IEnumerator RotateCouterClockWise()
+    {
+        while (true)
+        {
+            Debug.Log("rotate");
+            rotate++;
+            transform.rotation = Quaternion.Euler(0, 0, rotate);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    private IEnumerator RotateClockWise()
+    {
+        while (true)
+        {
+            Debug.Log("rotate");
+            rotate--;
+            transform.rotation = Quaternion.Euler(0, 0, rotate);
+            yield return new WaitForSeconds(0.02f);
+        }
     }
 }
