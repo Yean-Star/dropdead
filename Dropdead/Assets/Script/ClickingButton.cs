b130a56ff224149c5cb812a53487ab2124cc143a
Thinnapat <116701758+MellyWard@users.noreply.github.com>
2023-02-04 20:38:16 +0700

    Mainmenu Complete
    + Button Animation
    + Button Pressed
    + Interacted UI

---
+++ Dropdead/Assets/Script/ClickingButton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClickingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _default, _pressed;
+    [SerializeField] private AudioClip _compressClip, _uncompressClip;
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private Image raindrop;
+    //Detect current clicks on the GameObject (the one with the script attached)
+    void Start()
+    {
+        raindrop.enabled = false;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _img.sprite = _pressed;
+        raindrop.enabled = true;
+        Debug.Log(gameObject.name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _img.sprite = _default;
+        raindrop.enabled = false;
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        _img.sprite = _pressed;
+        _source.PlayOneShot(_compressClip);
+    }
+
+    //Detect if clicks are no longer registering
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        _img.sprite = _default;
+        _source.PlayOneShot(_uncompressClip);
+    }
+}
---
+++ Dropdead/Assets/Script/CreativeButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreativeButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button button;
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+        //button.transition.
+    }
+}
