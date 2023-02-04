3dab8418a393b79b7a81a0872b2e4ed5d0215257
Yean-Star <yeanhahaha@gmail.com>
2023-02-04 15:02:52 +0700

    Add Next Scene
    Add ending condition to change scene,
    Add game manager

---
+++ Dropdead/Assets/Script/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public void ChangeScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID,LoadSceneMode.Single);
+    }
+    
+}
