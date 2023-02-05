ff21ea62f6e7e7d4862bd88b69bbcb34e75570eb
Thinnapat <116701758+MellyWard@users.noreply.github.com>
2023-02-05 14:58:55 +0700

    splash scene + htp scene
    scene

--- Dropdead/Assets/Script/GameManager.cs
+++ Dropdead/Assets/Script/GameManager.cs
@@ -2,13 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject gameover;
     public AudioSource audioSource;
-    
+    public GameObject htpimg;
+
+    private void Awake()
+    {
+        if(htpimg != null)
+        {
+            htpimg.SetActive(false);
+        }
+        //htpimg.GetComponent<Image>().CrossFadeAlpha(0.1f, 2.0f, false);
+    }
+
+    private void Start()
+    {
+        //htpimg = GameObject.Find("HTP");
+    }
+
     public void Gameover()
     {
         Time.timeScale = 0;
@@ -29,5 +45,19 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void StartGame(int sceneID)
+    {
+        Time.timeScale = 1.0f;
+        StartCoroutine(HTPpanel(sceneID));
+    }
+
+    private IEnumerator HTPpanel(int sceneID)
+    {
+        htpimg.SetActive(true);
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene(sceneID, LoadSceneMode.Single);
+        
     }
 }
